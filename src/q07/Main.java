d09abaf62b124952f3d4ab21a9b8d88bfba7cf5a
Rishi Pal <rishi19092002@gmail.com>
2026-04-06 21:26:27 +0530

    Solved Q7
    Implemented bubble sort and input handling for an array of integers.

--- src/q07/Main.java
+++ src/q07/Main.java
@@ -1,22 +1,31 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int[] a = new int[n];
 
-        // TODO: Read n integers into an array
-        //       Implement BUBBLE SORT manually (no Arrays.sort)
-        //       Print sorted array ascending, space-separated
-        //       Then print: "Swaps: X" where X = total number of swaps performed
-        //
-        // Input:
-        // 5
-        // 64 34 25 12 22
-        //
-        // Output:
-        // 12 22 25 34 64
-        // Swaps: 7
+        for(int i=0;i<n;i++){
+            a[i] = sc.nextInt();
+        }
 
+        int swaps = 0;
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(a[j]>a[j+1]){
+                    int temp = a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=temp;
+                    swaps++;
+                }
+            }
+        }
+
+        for(int x:a){
+            System.out.print(x+" ");
+        }
+        System.out.println();
+        System.out.println(swaps);
     }
 }
