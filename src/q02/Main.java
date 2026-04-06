0f01bfb4683733de61ea981b5675f030a6d921e3
Rishi Pal <rishi19092002@gmail.com>
2026-04-06 23:01:26 +0530

    Solve Q2

--- src/q02/Main.java
+++ src/q02/Main.java
@@ -1,21 +1,23 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        int rows = 5; // Number of rows to print
-        
-        for (int i = 0; i < rows; i++) {
-            // Print leading spaces for pyramid structure
-            for (int j = 0; j < rows - i; j++) {
-                System.out.print(" ");
-            }
-            
-            int number = 1;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int num = 1;
+
             for (int j = 0; j <= i; j++) {
-                System.out.print(number + " ");
-                // Pascal's formula: C(n, k) = C(n, k-1) * (n-k+1) / k
-                number = number * (i - j) / (j + 1);
+                System.out.print(num);
+
+                if (j < i) {
+                    System.out.print(" ");
+                }
+
+                num = num * (i - j) / (j + 1);
             }
+
             System.out.println();
         }
     }
