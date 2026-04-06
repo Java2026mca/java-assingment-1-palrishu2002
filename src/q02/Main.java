923a3007af3f1d10b83dc15c8c4d9b62b6ddc94a
Rishi Pal <rishi19092002@gmail.com>
2026-04-06 22:49:18 +0530

    Fix q 2
    Refactored nested loops and removed unnecessary code.

--- src/q02/Main.java
+++ src/q02/Main.java
@@ -1,25 +1,19 @@
-public class Main {
-    public static void main(String[] args) {
+for (int i = 0; i < n; i++) {
+    int num = 1;
 
-        int[] tests = {1, 3, 5, 6};
+    for (int j = 0; j <= i; j++) {
+        System.out.print(num);
 
-        for (int n : tests) {
-
-            for (int i = 0; i < n; i++) {
-                int num = 1;
-
-                for (int j = 0; j <= i; j++) {
-                    System.out.print(num);
-
-                    if (j < i) {
-                        System.out.print(" ");
-                    }
-
-                    num = num * (i - j) / (j + 1);
-                }
-
-                System.out.println();
-            }
+        // space only between numbers
+        if (j < i) {
+            System.out.print(" ");
         }
+
+        num = num * (i - j) / (j + 1);
+    }
+
+    // new line (every row)
+    if (i < n - 1) {
+        System.out.println();
     }
 }
