e5aa2df82e90678e1725951cf5dbab1eebf0940a
Rishi Pal <rishi19092002@gmail.com>
2026-04-06 21:55:56 +0530

    Solved Q3

--- src/q03/Main.java
+++ src/q03/Main.java
@@ -2,33 +2,55 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // only n input
+
         int[][] a = new int[n][n];
 
-        // input matrix
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                a[i][j] = sc.nextInt();
+        int top = 0, bottom = n - 1;
+        int left = 0, right = n - 1;
+        int num = 1;
+
+        // Spiral fill
+        while (top <= bottom && left <= right) {
+
+            for (int i = left; i <= right; i++)
+                a[top][i] = num++;
+            top++;
+
+            for (int i = top; i <= bottom; i++)
+                a[i][right] = num++;
+            right--;
+
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--)
+                    a[bottom][i] = num++;
+                bottom--;
+            }
+
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--)
+                    a[i][left] = num++;
+                left++;
             }
         }
 
-        int sum = 0;
+        // Print matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(a[i][j]);
+                if (j < n - 1) System.out.print(" ");
+            }
+            System.out.println();
+        }
 
-        // primary diagonal
+        // Diagonal sum
+        int sum = 0;
         for (int i = 0; i < n; i++) {
             sum += a[i][i];
         }
 
-        // secondary diagonal
-        for (int i = 0; i < n; i++) {
-            if (i != n - i - 1) {
-                sum += a[i][n - i - 1];
-            }
-        }
-
-        // final output
-        System.out.print(sum);
+        System.out.println("Diagonal: " + sum);
     }
 }
