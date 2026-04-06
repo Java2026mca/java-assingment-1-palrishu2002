4d96fb7f2eefb2b77137ccdbdbc193f4dc9293cf
Rishi Pal <rishi19092002@gmail.com>
2026-04-06 21:23:54 +0530

    Solved Q3

--- src/q03/Main.java
+++ src/q03/Main.java
@@ -4,17 +4,22 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int[][] a = new int[n][n];
 
-        // TODO: Fill an N×N matrix in clockwise spiral order starting from 1
-        //       Print each row with values separated by single space
-        //       Then print: "Diagonal: X" where X = sum of primary diagonal (top-left to bottom-right)
-        //
-        // Input: 3
-        // Output:
-        // 1 2 3
-        // 8 9 4
-        // 7 6 5
-        // Diagonal: 15
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                a[i][j] = sc.nextInt();
+            }
+        }
 
+        int sum = 0;
+        for(int i=0;i<n;i++){
+            sum += a[i][i];
+            if(i != n-i-1){
+                sum += a[i][n-i-1];
+            }
+        }
+
+        System.out.println(sum);
     }
 }
