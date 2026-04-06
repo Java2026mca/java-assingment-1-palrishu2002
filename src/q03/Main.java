22c21c86c6ee3068eecc2187e236e4191d5e030f
Rishi Pal <rishi19092002@gmail.com>
2026-04-06 21:53:14 +0530

    Solved Q3
    Refactor matrix input and diagonal sum calculation.

--- src/q03/Main.java
+++ src/q03/Main.java
@@ -3,23 +3,32 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
         int[][] a = new int[n][n];
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
+        // input matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 a[i][j] = sc.nextInt();
             }
         }
 
         int sum = 0;
-        for(int i=0;i<n;i++){
+
+        // primary diagonal
+        for (int i = 0; i < n; i++) {
             sum += a[i][i];
-            if(i != n-i-1){
-                sum += a[i][n-i-1];
+        }
+
+        // secondary diagonal
+        for (int i = 0; i < n; i++) {
+            if (i != n - i - 1) {
+                sum += a[i][n - i - 1];
             }
         }
 
-        System.out.println(sum);
+        // final output
+        System.out.print(sum);
     }
 }
