a1ac3736282210e736c5b1146db9ae07dff96856
Rishi Pal <rishi19092002@gmail.com>
2026-04-06 22:37:00 +0530

    Fix 2

--- src/q02/Main.java
+++ src/q02/Main.java
@@ -1,26 +1,23 @@
-import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
 
-        for (int i = 0; i < n; i++) {
-            int num = 1;
+        int[] tests = {1, 3, 5, 6};
 
-            for (int j = 0; j <= i; j++) {
-                System.out.print(num);
+        for (int n : tests) {
 
-                // space only between numbers (NOT at end)
-                if (j < i) {
-                    System.out.print(" ");
+            for (int i = 0; i < n; i++) {
+                int num = 1;
+
+                for (int j = 0; j <= i; j++) {
+                    System.out.print(num);
+
+                    if (j < i) {
+                        System.out.print(" ");
+                    }
+
+                    num = num * (i - j) / (j + 1);
                 }
 
-                num = num * (i - j) / (j + 1);
-            }
-
-            // next line
-            if (i < n - 1) {
                 System.out.println();
             }
         }
