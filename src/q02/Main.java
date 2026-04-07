a7a127bdd8671597ffcea7b7d396c940b60b33f4
Rishi Pal <rishi19092002@gmail.com>
2026-04-07 22:06:35 +0530

    Solved Q02 Pascal Triangle

--- src/q02/Main.java
+++ src/q02/Main.java
@@ -1,33 +1,21 @@
-package q02;
-
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        // Input
         int n = sc.nextInt();
 
-        // Pascal's Triangle Logic
         for (int i = 0; i < n; i++) {
-
-            // Spaces (for proper triangle shape)
-            for (int j = 0; j < n - i - 1; j++) {
-                System.out.print(" ");
-            }
-
             int num = 1;
 
-            // Print values
             for (int j = 0; j <= i; j++) {
-                System.out.print(num + " ");
+                System.out.print(num);
+
+                if (j < i) System.out.print(" ");
+
                 num = num * (i - j) / (j + 1);
             }
-
             System.out.println();
         }
-
-        sc.close();
     }
 }
