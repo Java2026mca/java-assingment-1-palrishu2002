32df100f8192988492811116234bb0cf66afe9a9
Rishi Pal <rishi19092002@gmail.com>
2026-04-07 11:25:12 +0530

    Solve q3 again

--- src/q03/Main.java
+++ src/q03/Main.java
@@ -45,81 +45,19 @@ public class Main {
             System.out.println();
         }
 
-        // Diagonal sum (BOTH diagonals)
+        // Diagonal sum
         int sum = 0;
 
         for (int i = 0; i < n; i++) {
-            sum += a[i][i];               // primary
-            sum += a[i][n - i - 1];       // secondary
+            sum += a[i][i];
+            sum += a[i][n - i - 1];
         }
 
-        // Remove double-counted center (odd n)
         if (n % 2 == 1) {
             sum -= a[n/2][n/2];
         }
 
-        System.out.println("Diagonal: " + sum);
-    }
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-
-        int[][] a = new int[n][n];
-
-        int top = 0, bottom = n - 1;
-        int left = 0, right = n - 1;
-        int num = 1;
-
-        // Spiral fill
-        while (top <= bottom && left <= right) {
-
-            for (int i = left; i <= right; i++)
-                a[top][i] = num++;
-            top++;
-
-            for (int i = top; i <= bottom; i++)
-                a[i][right] = num++;
-            right--;
-
-            if (top <= bottom) {
-                for (int i = right; i >= left; i--)
-                    a[bottom][i] = num++;
-                bottom--;
-            }
-
-            if (left <= right) {
-                for (int i = bottom; i >= top; i--)
-                    a[i][left] = num++;
-                left++;
-            }
-        }
-
-        // Print matrix
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.print(a[i][j]);
-                if (j < n - 1) System.out.print(" ");
-            }
-            System.out.println();
-        }
-
-        // Diagonal sum (BOTH diagonals)
-        int sum = 0;
-
-        for (int i = 0; i < n; i++) {
-            sum += a[i][i];               // primary
-            sum += a[i][n - i - 1];       // secondary
-        }
-
-        // Remove double-counted center (odd n)
-        if (n % 2 == 1) {
-            sum -= a[n/2][n/2];
-        }
-
-        System.out.println("Diagonal: " + sum);
+        // ⚠️ IMPORTANT CHANGE
+        System.out.println(sum);   // remove "Diagonal: "
     }
 }
