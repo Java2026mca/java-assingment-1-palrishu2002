a86648ad2feaadcd056d1969a1d880c5f4da9c3f
Rishi Pal <rishi19092002@gmail.com>
2026-04-07 22:39:34 +0530

    FIX 7
    Refactor variable names and improve bubble sort implementation. Added formatted output for swap count.

--- src/q07/Main.java
+++ src/q07/Main.java
@@ -1,31 +1,39 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] a = new int[n];
 
-        for(int i=0;i<n;i++){
-            a[i] = sc.nextInt();
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
         }
 
         int swaps = 0;
-        for(int i=0;i<n-1;i++){
-            for(int j=0;j<n-i-1;j++){
-                if(a[j]>a[j+1]){
-                    int temp = a[j];
-                    a[j]=a[j+1];
-                    a[j+1]=temp;
+
+        // Bubble Sort
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                     swaps++;
                 }
             }
         }
 
-        for(int x:a){
-            System.out.print(x+" ");
+        // Print sorted array (NO extra space)
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i]);
+            if (i < n - 1) System.out.print(" ");
         }
         System.out.println();
-        System.out.println(swaps);
+
+        // Correct format
+        System.out.println("Swaps: " + swaps);
     }
 }
