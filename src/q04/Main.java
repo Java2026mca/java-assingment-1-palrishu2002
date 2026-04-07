34ef5ebdd2e2b0d777c884b7830874e0a8937cf5
Rishi Pal <rishi19092002@gmail.com>
2026-04-07 22:21:44 +0530

    FIX Q 4

--- src/q04/Main.java
+++ src/q04/Main.java
@@ -3,41 +3,41 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
 
-        // Check Prime
-        boolean isPrime = true;
-        if (n <= 1) {
-            isPrime = false;
-        } else {
-            for (int i = 2; i * i <= n; i++) {
-                if (n % i == 0) {
-                    isPrime = false;
-                    break;
+        // Multiple test cases handle
+        while (sc.hasNextInt()) {
+            int n = sc.nextInt();
+
+            // Check Prime
+            boolean isPrime = true;
+            if (n <= 1) {
+                isPrime = false;
+            } else {
+                for (int i = 2; i * i <= n; i++) {
+                    if (n % i == 0) {
+                        isPrime = false;
+                        break;
+                    }
                 }
             }
-        }
 
-        // Check Perfect
-        int sum = 0;
-        for (int i = 1; i <= n / 2; i++) {
-            if (n % i == 0) {
-                sum += i;
+            // Check Perfect
+            int sum = 0;
+            for (int i = 1; i <= n / 2; i++) {
+                if (n % i == 0) {
+                    sum += i;
+                }
             }
-        }
-        boolean isPerfect = (sum == n && n != 0);
+            boolean isPerfect = (sum == n && n != 0);
 
-        // Output (VERY IMPORTANT FORMAT)
-        if (isPrime) {
-            System.out.println("Prime");
-        } else {
-            System.out.println("Not Prime");
-        }
-
-        if (isPerfect) {
-            System.out.println("Perfect");
-        } else {
-            System.out.println("Not Perfect");
+            // FINAL OUTPUT (IMPORTANT)
+            if (isPrime) {
+                System.out.println("Prime");
+            } else if (isPerfect) {
+                System.out.println("Perfect");
+            } else {
+                System.out.println("Neither");
+            }
         }
     }
 }
