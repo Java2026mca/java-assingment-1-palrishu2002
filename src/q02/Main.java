9f764b44ccb070745b55694970acda6cdad8928d
Rishi Pal <rishi19092002@gmail.com>
2026-04-07 21:55:29 +0530

    Solved Q2

--- src/q02/Main.java
+++ src/q02/Main.java
@@ -1,22 +1,25 @@
+package q02;
+
 import java.util.Scanner;
 
-public class PascalsTriangle {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter number of rows: ");
+        // Input
         int n = sc.nextInt();
 
+        // Pascal's Triangle Logic
         for (int i = 0; i < n; i++) {
 
-            // Print spaces
-            for (int j = 0; j < n - i; j++) {
+            // Spaces (for proper triangle shape)
+            for (int j = 0; j < n - i - 1; j++) {
                 System.out.print(" ");
             }
 
             int num = 1;
 
-            // Print numbers
+            // Print values
             for (int j = 0; j <= i; j++) {
                 System.out.print(num + " ");
                 num = num * (i - j) / (j + 1);
