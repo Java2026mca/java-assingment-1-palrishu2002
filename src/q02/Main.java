bf46cef0d9f7e9da11400057a7806a18909c709e
Rishi Pal <rishi19092002@gmail.com>
2026-04-07 21:52:48 +0530

    Solved Q2
    Refactored the code to print Pascal's Triangle instead of filling a matrix in spiral order. Removed unused variables and added logic to calculate and print the diagonal sum.

--- src/q02/Main.java
+++ src/q02/Main.java
@@ -1,73 +1,30 @@
-import java.util.*;
+import java.util.Scanner;
 
-public class Main {
+public class PascalsTriangle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
         int n = sc.nextInt();
-        int[][] arr = new int[n][n];
 
-int top = 0, bottom = n - 1;
-int left = 0, right = n - 1;
-int num = 1;
+        for (int i = 0; i < n; i++) {
 
-// Fill spiral
-while (top <= bottom && left <= right) {
+            // Print spaces
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
 
-    // left → right
-    for (int i = left; i <= right; i++) {
-        arr[top][i] = num++;
-    }
-    top++;
+            int num = 1;
 
-    // top → bottom
-    for (int i = top; i <= bottom; i++) {
-        arr[i][right] = num++;
-    }
-    right--;
+            // Print numbers
+            for (int j = 0; j <= i; j++) {
+                System.out.print(num + " ");
+                num = num * (i - j) / (j + 1);
+            }
 
-    // right → left
-    if (top <= bottom) {
-        for (int i = right; i >= left; i--) {
-            arr[bottom][i] = num++;
+            System.out.println();
         }
-        bottom--;
-    }
 
-    // bottom → top
-    if (left <= right) {
-        for (int i = bottom; i >= top; i--) {
-            arr[i][left] = num++;
-        }
-        left++;
-    }
-}
-
-// Print matrix
-for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-        System.out.print(arr[i][j]);
-        if (j < n - 1) System.out.print(" ");
-    }
-    System.out.println();
-}
-
-// Diagonal sum
-int sum = 0;
-for (int i = 0; i < n; i++) {
-    sum += arr[i][i];
-}
-
-System.out.println("Diagonal: " + sum);
-        // TODO: Fill an N×N matrix in clockwise spiral order starting from 1
-        //       Print each row with values separated by single space
-        //       Then print: "Diagonal: X" where X = sum of primary diagonal (top-left to bottom-right)
-        //
-        // Input: 3
-        // Output:
-        // 1 2 3
-        // 8 9 4
-        // 7 6 5
-        // Diagonal: 15
-
+        sc.close();
     }
 }
