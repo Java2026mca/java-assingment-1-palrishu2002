3d94800223ed356c3a85b056d79f7a2871b6087c
Rishi Pal <rishi19092002@gmail.com>
2026-04-07 21:40:23 +0530

    Solved Q2
    Implemented a method to fill an N×N matrix in clockwise spiral order starting from 1, print each row with values separated by a space, and calculate the sum of the primary diagonal.

--- src/q02/Main.java
+++ src/q02/Main.java
@@ -4,21 +4,70 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int[][] arr = new int[n][n];
 
-        for (int i = 0; i < n; i++) {
-            int num = 1;
+int top = 0, bottom = n - 1;
+int left = 0, right = n - 1;
+int num = 1;
 
-            for (int j = 0; j <= i; j++) {
-                System.out.print(num);
+// Fill spiral
+while (top <= bottom && left <= right) {
 
-                if (j < i) {
-                    System.out.print(" ");
-                }
+    // left → right
+    for (int i = left; i <= right; i++) {
+        arr[top][i] = num++;
+    }
+    top++;
 
-                num = num * (i - j) / (j + 1);
-            }
+    // top → bottom
+    for (int i = top; i <= bottom; i++) {
+        arr[i][right] = num++;
+    }
+    right--;
 
-            System.out.println();
+    // right → left
+    if (top <= bottom) {
+        for (int i = right; i >= left; i--) {
+            arr[bottom][i] = num++;
         }
+        bottom--;
+    }
+
+    // bottom → top
+    if (left <= right) {
+        for (int i = bottom; i >= top; i--) {
+            arr[i][left] = num++;
+        }
+        left++;
+    }
+}
+
+// Print matrix
+for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+        System.out.print(arr[i][j]);
+        if (j < n - 1) System.out.print(" ");
+    }
+    System.out.println();
+}
+
+// Diagonal sum
+int sum = 0;
+for (int i = 0; i < n; i++) {
+    sum += arr[i][i];
+}
+
+System.out.println("Diagonal: " + sum);
+        // TODO: Fill an N×N matrix in clockwise spiral order starting from 1
+        //       Print each row with values separated by single space
+        //       Then print: "Diagonal: X" where X = sum of primary diagonal (top-left to bottom-right)
+        //
+        // Input: 3
+        // Output:
+        // 1 2 3
+        // 8 9 4
+        // 7 6 5
+        // Diagonal: 15
+
     }
 }
