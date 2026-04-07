37d371ae27743d9b6c18ba53a02755486c819462
Rishi Pal <rishi19092002@gmail.com>
2026-04-07 22:28:55 +0530

    Solved Q05 Shape Area
    Removed prime and perfect number checks and added area calculations for circle, rectangle, and triangle based on user input.

--- src/q04/Main.java
+++ src/q04/Main.java
@@ -4,41 +4,24 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int t = sc.nextInt(); // number of test cases
+        String shape = sc.next();
 
-        while (t-- > 0) {
-            int n = sc.nextInt();
-
-            // Prime check
-            boolean isPrime = true;
-            if (n <= 1) {
-                isPrime = false;
-            } else {
-                for (int i = 2; i * i <= n; i++) {
-                    if (n % i == 0) {
-                        isPrime = false;
-                        break;
-                    }
-                }
-            }
-
-            // Perfect check
-            int sum = 0;
-            for (int i = 1; i <= n / 2; i++) {
-                if (n % i == 0) {
-                    sum += i;
-                }
-            }
-            boolean isPerfect = (sum == n);
-
-            // FINAL OUTPUT
-            if (isPerfect) {
-                System.out.println("Perfect");
-            } else if (isPrime) {
-                System.out.println("Prime");
-            } else {
-                System.out.println("Neither");
-            }
+        if (shape.equalsIgnoreCase("circle")) {
+            double r = sc.nextDouble();
+            double area = Math.PI * r * r;
+            System.out.printf("%.2f\n", area);
+        } 
+        else if (shape.equalsIgnoreCase("rectangle")) {
+            double l = sc.nextDouble();
+            double b = sc.nextDouble();
+            double area = l * b;
+            System.out.printf("%.2f\n", area);
+        } 
+        else if (shape.equalsIgnoreCase("triangle")) {
+            double base = sc.nextDouble();
+            double height = sc.nextDouble();
+            double area = 0.5 * base * height;
+            System.out.printf("%.2f\n", area);
         }
     }
 }
