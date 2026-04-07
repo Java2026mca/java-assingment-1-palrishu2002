b4e81e3851dee36eefe9903be0ea3603a4b1fb65
Rishi Pal <rishi19092002@gmail.com>
2026-04-07 22:35:41 +0530

    Refactor Main.java to check for prime and perfect numbersFIX 4
    Removed shape area calculations and added prime/perfect number checks.

--- src/q04/Main.java
+++ src/q04/Main.java
@@ -4,24 +4,53 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String shape = sc.next();
+        // Agar t diya hai to bhi chalega, nahi diya to bhi
+        List<Integer> list = new ArrayList<>();
 
-        if (shape.equalsIgnoreCase("circle")) {
-            double r = sc.nextDouble();
-            double area = Math.PI * r * r;
-            System.out.printf("%.2f\n", area);
-        } 
-        else if (shape.equalsIgnoreCase("rectangle")) {
-            double l = sc.nextDouble();
-            double b = sc.nextDouble();
-            double area = l * b;
-            System.out.printf("%.2f\n", area);
-        } 
-        else if (shape.equalsIgnoreCase("triangle")) {
-            double base = sc.nextDouble();
-            double height = sc.nextDouble();
-            double area = 0.5 * base * height;
-            System.out.printf("%.2f\n", area);
+        while (sc.hasNextInt()) {
+            list.add(sc.nextInt());
+        }
+
+        int start = 0;
+
+        // Agar first value test cases ho sakti hai
+        if (list.size() > 1 && list.get(0) == list.size() - 1) {
+            start = 1;
+        }
+
+        for (int i = start; i < list.size(); i++) {
+            int n = list.get(i);
+
+            // Prime check
+            boolean isPrime = true;
+            if (n <= 1) {
+                isPrime = false;
+            } else {
+                for (int j = 2; j * j <= n; j++) {
+                    if (n % j == 0) {
+                        isPrime = false;
+                        break;
+                    }
+                }
+            }
+
+            // Perfect check
+            int sum = 0;
+            for (int j = 1; j <= n / 2; j++) {
+                if (n % j == 0) {
+                    sum += j;
+                }
+            }
+            boolean isPerfect = (sum == n);
+
+            // Output (exact match)
+            if (isPerfect) {
+                System.out.println("Perfect");
+            } else if (isPrime) {
+                System.out.println("Prime");
+            } else {
+                System.out.println("Neither");
+            }
         }
     }
 }
