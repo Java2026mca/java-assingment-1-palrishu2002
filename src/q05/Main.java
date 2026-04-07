1fafc3e53719345410d1c95744a996d789e19ee2
Rishi Pal <rishi19092002@gmail.com>
2026-04-07 22:33:05 +0530

    Update Main.java

--- src/q05/Main.java
+++ src/q05/Main.java
@@ -9,19 +9,24 @@ public class Main {
         if (shape.equalsIgnoreCase("circle")) {
             double r = sc.nextDouble();
             double area = Math.PI * r * r;
-            System.out.printf("%.2f\n", area);
+            System.out.printf("Area: %.2f\n", area);
         } 
         else if (shape.equalsIgnoreCase("rectangle")) {
             double l = sc.nextDouble();
             double b = sc.nextDouble();
             double area = l * b;
-            System.out.printf("%.2f\n", area);
+            System.out.printf("Area: %.2f\n", area);
         } 
         else if (shape.equalsIgnoreCase("triangle")) {
-            double base = sc.nextDouble();
-            double height = sc.nextDouble();
-            double area = 0.5 * base * height;
-            System.out.printf("%.2f\n", area);
+            // 3 sides diye ja rahe hain (Heron's Formula)
+            double a = sc.nextDouble();
+            double b = sc.nextDouble();
+            double c = sc.nextDouble();
+
+            double s = (a + b + c) / 2.0;
+            double area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+
+            System.out.printf("Area: %.2f\n", area);
         }
     }
 }
