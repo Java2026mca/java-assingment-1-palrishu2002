ce85697aec918ff978c05793f723506e57568ba3
Rishi Pal <rishi19092002@gmail.com>
2026-04-08 08:51:39 +0530

    finallly

--- src/q07/Main.java
+++ src/q07/Main.java
@@ -4,50 +4,40 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Input dimensions
-        int r1 = sc.nextInt();
-        int c1 = sc.nextInt();
-        int r2 = sc.nextInt();
-        int c2 = sc.nextInt();
+        int n = sc.nextInt();   // only ONE size
 
-        // Check multiplication condition
-        if (c1 != r2) {
-            System.out.println("Matrix multiplication not possible");
-            return;
-        }
+        int[][] A = new int[n][n];
+        int[][] B = new int[n][n];
 
-        // Input first matrix
-        int[][] A = new int[r1][c1];
-        for (int i = 0; i < r1; i++) {
-            for (int j = 0; j < c1; j++) {
+        // input matrix A
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
                 A[i][j] = sc.nextInt();
             }
         }
 
-        // Input second matrix
-        int[][] B = new int[r2][c2];
-        for (int i = 0; i < r2; i++) {
-            for (int j = 0; j < c2; j++) {
+        // input matrix B
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
                 B[i][j] = sc.nextInt();
             }
         }
 
-        // Result matrix
-        int[][] C = new int[r1][c2];
+        int[][] C = new int[n][n];
 
-        // Matrix multiplication
-        for (int i = 0; i < r1; i++) {
-            for (int j = 0; j < c2; j++) {
-                for (int k = 0; k < c1; k++) {
+        // multiplication
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                for(int k = 0; k < n; k++) {
                     C[i][j] += A[i][k] * B[k][j];
                 }
             }
         }
 
-        // Print result matrix
-        for (int i = 0; i < r1; i++) {
-            for (int j = 0; j < c2; j++) {
-                if (j > 0) System.out.print(" ");
+        // print result (NO extra space)
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(j > 0) System.out.print(" ");
                 System.out.print(C[i][j]);
             }
             System.out.println();
