1bb7e2bd291ef888550c0bd0326267c2ea8b6970
Rishi Pal <rishi19092002@gmail.com>
2026-04-08 09:03:29 +0530

    Update Main.java q 7

--- src/q07/Main.java
+++ src/q07/Main.java
@@ -13,15 +13,21 @@ public class Main {
 
         int swapCount = 0;
 
-        // Standard Bubble Sort (adjacent swaps count)
+        // Selection Sort (minimum swaps)
         for(int i = 0; i < n - 1; i++) {
-            for(int j = 0; j < n - i - 1; j++) {
-                if(arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                    swapCount++;
+            int minIndex = i;
+
+            for(int j = i + 1; j < n; j++) {
+                if(arr[j] < arr[minIndex]) {
+                    minIndex = j;
                 }
+            }
+
+            if(minIndex != i) {
+                int temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
+                swapCount++;
             }
         }
 
