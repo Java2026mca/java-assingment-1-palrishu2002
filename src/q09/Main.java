53e5ca3575eb439a3cf4dc9759b3ed9e1ac23b04
Rishi Pal <rishi19092002@gmail.com>
2026-04-08 08:54:38 +0530

    q 10 final

--- src/q09/Main.java
+++ src/q09/Main.java
@@ -1,32 +1,59 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int r1 = sc.nextInt(), c1 = sc.nextInt();
-        int[][] A = new int[r1][c1];
+        int n = sc.nextInt();
+       int[][] A = new int[n][n];
+int[][] B = new int[n][n];
+int[][] C = new int[n][n];
 
-        for(int i=0;i<r1;i++)
-            for(int j=0;j<c1;j++)
-                A[i][j] = sc.nextInt();
+// Input matrix A
+for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+        A[i][j] = sc.nextInt();
+    }
+}
 
-        int r2 = sc.nextInt(), c2 = sc.nextInt();
-        int[][] B = new int[r2][c2];
+// Input matrix B
+for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+        B[i][j] = sc.nextInt();
+    }
+}
 
-        for(int i=0;i<r2;i++)
-            for(int j=0;j<c2;j++)
-                B[i][j] = sc.nextInt();
-
-        int[][] C = new int[r1][c2];
-
-        for(int i=0;i<r1;i++){
-            for(int j=0;j<c2;j++){
-                for(int k=0;k<c1;k++){
-                    C[i][j] += A[i][k]*B[k][j];
-                }
-                System.out.print(C[i][j]+" ");
-            }
-            System.out.println();
+// Multiply A and B → C
+for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+        C[i][j] = 0;
+        for (int k = 0; k < n; k++) {
+            C[i][j] += A[i][k] * B[k][j];
         }
+    }
+}
+
+// Print result matrix
+for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+        System.out.print(C[i][j]);
+        if (j < n - 1) System.out.print(" ");
+    }
+    System.out.println();
+}
+        // TODO: Read two n×n matrices A and B, multiply them → print result C = A×B
+        //       Input: matrix A row by row, then matrix B row by row
+        //       Output: result matrix row by row, values separated by spaces
+        //
+        // Input:
+        // 2
+        // 1 2
+        // 3 4
+        // 5 6
+        // 7 8
+        //
+        // Output:
+        // 19 22
+        // 43 50
+
     }
 }
